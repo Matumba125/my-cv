1afced8e452f8c175352c864cba64be97a58a333
matumba 125 <nbenza5@gmail.com>
2021-07-29 13:48:44 +0300

    fixed url for social network

--- src/projects/Projects.js
+++ src/projects/Projects.js
@@ -13,7 +13,7 @@ function Projects() {
                     <Project
                         skillName={'Social Network'}
                         description={'network'}
-                        projectLink={'https://preview.themeforest.net/item/jeffrey-personal-portfolio-template/full_screen_preview/24187700?_ga=2.18315711.763890385.1563976908-1245260935.1563976797'}/>
+                        projectLink={'https://matumba125.github.io/social-network/'}/>
                     <Project skillName={'Todolist'}
                              description={'list'}
                              projectLink={'https://matumba125.github.io/todolist/'}/>
